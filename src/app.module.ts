4eca15dbb81efb3f27082af5bc26209b832e84b9
mikeyudex <miguel.yudex@gmail.com>
2025-07-22 18:00:39 -0500

    feat: add Customer module with controller, service, and schema; update app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { ControlDocumentalModule } from './control-documental/control-documental.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [VehiclesModule, ControlDocumentalModule],
+  imports: [VehiclesModule, ControlDocumentalModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/customer/customer.schema.ts
@@ -0,0 +1,53 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { getCurrentUTCDate } from '../utils/getUtcDate';
+import { Document } from 'mongoose';
+
+export type CustomerDocument = Customer & Document;
+
+@Schema()
+export class Customer {
+
+    @Prop({ required: true, type: String })
+    nombres: string;
+
+    @Prop({ required: true, type: String })
+    apellidos: string;
+
+    @Prop({ required: true, type: String })
+    rut: string;
+
+    @Prop({ required: true, type: String })
+    direccion: string;
+
+    @Prop({ required: true, type: String })
+    email: string;
+
+    @Prop({ required: true, type: String })
+    telefono: string;
+
+    @Prop({ required: false, type: String })
+    fotoCi: string;
+
+    @Prop({ required: false, type: String })
+    fotoLicencia: string;
+
+    @Prop({ required: false, type: String })
+    hojaAntecedentes: string;
+
+    @Prop({ required: false, type: String })
+    hojaConductor: string;
+
+    @Prop({ required: false, type: String })
+    eRut: string;
+
+    @Prop({ required: true, type: String })
+    contratoUrl: string;
+
+    @Prop({ default: () => getCurrentUTCDate() })
+    createdAt: Date;
+
+    @Prop({ default: null })
+    updatedAt: Date;
+}
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
